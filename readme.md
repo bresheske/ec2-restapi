40053eda3f7ddf62a7296c73ddeb26a32fa6ce68
brandon <bresheske@gmail.com>
2019-04-27 14:30:29 -0500

    initial

---
+++ readme.md
@@ -0,0 +1,12 @@
+# To Run Development
+```
+npm i
+npm start
+```
+
+# To Deploy
+ - Have OpenSSH installed
+ - Have an EC2 instance running in AWS
+ - Allow the instance, with a Security Group, inbound to your port.
+ - Have your pem file from the EC2 instance
+ - Run `npm run deploy`.
---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+const config = require('./config.json');
+
+// create our webapp, and configure it to be JSON.
+const app = express();
+app.use(express.json());
+
+// handle all of our routes
+app.get('/', (request, response) => {
+    return response.json({
+        message: "hi."
+    });
+});
+
+// start up our service
+app.listen(config.port, () =>
+    console.log(`Example app listening on port ${config.port}!`),
+);
